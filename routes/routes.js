cbf4bb59d1eb6cfc80241005e01bed2ba198caae
santhosh <singumahanthisanthosh@gmail.com>
2018-10-10 17:09:00 +0530

    changes in routes made

--- routes/routes.js
+++ routes/routes.js
@@ -7,41 +7,16 @@ const bcryptJs = require("bcryptjs");
 const User = require('../model/user');
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
-router.get('/home', (req, res, next) => {
-    res.render('home');
-});
-router.get('/', (req, res, next) => {
-    res.render('app');
-});
-router.get('/app', (req, res, next) => {
+
+router.get('/Food', (req, res, next) => {
     res.json({'msg':'hi niki'});
 });
-router.post('/search',(req,res)=>{
+router.get('/Cloathing',(req,res)=>{
 
     res.json({"msg":" Welcome to Search route!"});
 });
-router.post('/slide',(req,res)=>{
+router.get('/Footwear',(req,res)=>{
 
     res.json({"msg":" Welcome to Image slider route!"});
-});
-router.post('/search',(req,res)=>{
-
-    res.json({"msg":" Welcome to Search route!"});
-});
-router.post('/deals',(req,res)=>{
-
-    res.json({"msg":" Welcome to deals of the day route!"});
-});
-router.post('/cart',(req,res)=>{
-
-    res.json({"msg":" Welcome to cart route!"});
-});
-router.post('/sellers',(req,res)=>{
-
-    res.json({"msg":" Welcome to Sellers route!"});
-});
-router.post('/pay',(req,res)=>{
-
-    res.json({"msg":" Welcome to payment route!"});
 });
 module.exports = router;
