14f902f56bb3ed3e3dcf6fc754c3016875985d7b
nikhita <nikhitazulka@gmail.com>
2018-10-12 18:11:23 +0530

    Updated the login route

--- e-comm/src/Reducers/Reducer.js
+++ e-comm/src/Reducers/Reducer.js
@@ -60,18 +60,19 @@ export function login(username, password){
         dispatch(setLoginPending(true));
         dispatch(setLoginSuccess(false, null));
         dispatch(setLoginError(null));
-        var bodyFormData = new FormData();
-        bodyFormData.set('userName', username);
-       sendLoginRequest(username,password)
+       // var bodyFormData = new FormData();
+       // bodyFormData.set('userName', username);
+       //sendLoginRequest(username,password)
        //checking whether the user exists or not in DB
-        // Axios.get('https://e-commerce-application.herokuapp.com/login', bodyFormData)
-        
-        // Axios({
-        //     url: 'https://e-commerce-application.herokuapp.com/login',
-        //     method: 'get',
-        //     headers: {'Content-Type': 'application/json'},
-        //     data: {"fhasjkhfa": "kaljfiljn"},
-        // })
+       // Axios.get('https://e-commerce-application.herokuapp.com/login',{username, password} )
+        //login should be a post request and not a get request
+        Axios({
+            url: 'https://e-commerce-application.herokuapp.com/login',
+            method: 'post',
+            //headers: {'Content-Type': 'application/json'},
+            data: {username: username,
+                    password: password},
+        })
         .then(userData =>{
             console.log("USER DATA", userData)
             dispatch(setLoginPending(false));
@@ -130,49 +131,49 @@ export default function reducer(state={
 }
 
 
-function sendLoginRequest(username, password){
-    //var result={}
-    //console.log("##################", file)
-    // old method of fetching data from json file
-    // fetch(file)
-    // .then((response) => {
-    //     console.log(response)
-    //     return response.json()
+// function sendLoginRequest(username, password){
+//     //var result={}
+//     //console.log("##################", file)
+//     // old method of fetching data from json file
+//     // fetch(file)
+//     // .then((response) => {
+//     //     console.log(response)
+//     //     return response.json()
         
-    // })
-    // .then((findresponse) =>{
-    //     result= findresponse
-    //     console.log("loginrequest fxn ", result)
-    //  })
-    //  .catch((err) =>{
-    //         console.log(err)
-    //       });
+//     // })
+//     // .then((findresponse) =>{
+//     //     result= findresponse
+//     //     console.log("loginrequest fxn ", result)
+//     //  })
+//     //  .catch((err) =>{
+//     //         console.log(err)
+//     //       });
 
-    return new Promise ((resolve, reject) => {
-        var flag= false
-        console.log(file.contacts)
-        file.contacts.forEach((user) => {
-            console.log(user.email+"*********************")
-            console.log(username+"&&&&&&&&&&&&&&&&&&")
-            if(username ===user.email){
-                //try to use indexOf
-                if(password === user.password){
-                    console.log(user)
-                    return resolve(user)
-                }
-                else{
-                    return reject(new Error("Invalid password"))
-                }
-            }
-            else{// check with flags
-               flag= true
-            }
+//     return new Promise ((resolve, reject) => {
+//         var flag= false
+//         console.log(file.contacts)
+//         file.contacts.forEach((user) => {
+//             console.log(user.email+"*********************")
+//             console.log(username+"&&&&&&&&&&&&&&&&&&")
+//             if(username ===user.email){
+//                 //try to use indexOf
+//                 if(password === user.password){
+//                     console.log(user)
+//                     return resolve(user)
+//                 }
+//                 else{
+//                     return reject(new Error("Invalid password"))
+//                 }
+//             }
+//             else{// check with flags
+//                flag= true
+//             }
            
-        })
-        if(flag){
-            console.log("In Reducer value is "+flag)
-            return reject(new Error("Invalid username"))
-        }
+//         })
+//         if(flag){
+//             console.log("In Reducer value is "+flag)
+//             return reject(new Error("Invalid username"))
+//         }
        
-    })
-}
+//     })
+// }
